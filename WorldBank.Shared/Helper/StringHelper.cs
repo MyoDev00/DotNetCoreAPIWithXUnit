6f8ae96e59292bd5503c62a25b35220ea8f7bd80
MyoDev00 <myodev00@gmail.com>
2022-09-13 21:30:59 +0700

    bugfix
    feature : UnitTest : remove sensitive data from DB Fixture
    feature : Share : add string helper

---
+++ WorldBank.Shared/Helper/StringHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldBank.Shared.Helper
+{
+    public static class StringHelper
+    {
+        public static string Encrypt(string text,string encryptionKey)
+        {
+            return ECBEnvryption.Encrypt(text, encryptionKey);
+        }
+
+        public static string Decrypt(string hash, string encryptionKey)
+        {
+            return ECBEnvryption.Decrypt(hash, encryptionKey);
+        }
+    }
+}
